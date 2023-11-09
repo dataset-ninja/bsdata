60ff8682a6855f1281c44babc51f51afa58d077d
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-09 13:51:45 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -14,7 +14,7 @@ from dataset_tools.templates import (
 # * Before uploading to instance #
 ##################################
 PROJECT_NAME: str = "BSData"
-PROJECT_NAME_FULL: str = "BSData: Dataset for Instance Segmentation and Industrial Wear Forecasting"
+PROJECT_NAME_FULL: str = "BSData: Industrial Machine Tool Element Surface Defect Dataset"
 HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
